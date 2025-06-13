faf00dac6470d21ebbb408c67bc8c8155278ff88
flyerix <154899257+flyerix@users.noreply.github.com>
2025-06-13 06:49:04 +0200

    Update deepseek_python_20250610_ca2dcb.py

--- deepseek_python_20250610_ca2dcb.py
+++ deepseek_python_20250610_ca2dcb.py
@@ -22,8 +22,8 @@ from telegram.ext import (
 )
 
 # Configurazione
-TOKEN = os.getenv("AAGYlO-aF-dK10bpAlf2ZViFOKLgc2wWHI4")
-ADMIN_ID = int(os.getenv("691735614"))
+TOKEN = os.getenv("TELEGRAM_TOKEN")
+ADMIN_ID = int(os.getenv("ADMIN_CHAT_ID"))
 DB_NAME = "database.db"
 COSTO_MENSILE = 15  # €15 al mese
 
